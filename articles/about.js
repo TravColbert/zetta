136f922bd029acd0089d609c01914cd148a8e7fc
Trav Colbert <trav.colbert@gmail.com>
2026-08-15 21:14:05 -0700

    Corrected case for Markdown

--- articles/about.js
+++ articles/about.js
@@ -20,13 +20,13 @@ Redeploying is just a webhook.
 
 ## Low Attack Surface, Runs Anywhere
 
-As of this writing Zetta has exactly ONE dependency: the **[marked](https://github.com/markedjs/marked)** Markdown renderer.
+As of this writing Zetta has exactly ONE dependency: the **[Marked](https://github.com/markedjs/marked)** Markdown renderer.
 
-Everything else is just vanilla **[Bun](https://bun.com/)**.
+Everything else is just vanilla **JavaScript/[Bun](https://bun.com/)**.
 
 There's no ORM, no bundler.
 
-Our goal was that you should be able to run Zetta anywhere. In a container. On a host. In a box. On a boat...
+Our goal was that you should be able to run Zetta anywhere. In a container. On a host. In a moat. On a boat...
 
 ## Comprehensible and Transparent
 
